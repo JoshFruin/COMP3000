697689d2f3e02c256ca55517fc56792fdf2e2e76
Josh Fruin <72512025+JoshFruin@users.noreply.github.com>
2022-11-04 15:53:16 +0000

    Starting Version 1 of the network Scanner

---
+++ Network Scanner.py
@@ -0,0 +1,24 @@
+
+import nmap
+
+Scanner = nmap.PortScanner()
+
+print("Network Scanner V1")
+
+ip_add = input("Please enter the IP address which you want to scan: ")
+
+ScanType = input ("""\n Please enter the type of scan you wish to run
+                  1)SYN ACK Scan
+                  2)UDP Scan
+                  3)Comprehensive Scan \n""")
+print("You have selected option: ", ScanType)
+
+if ScanType == '1':
+    print("Nmap Version: ", Scanner.nmap_version())
+    Scanner.scan(ip_add, '1-1024', '-v -sS')
+    print(Scanner.scaninfo())
+    print("IP Status: ", Scanner[ip_add].state())
+    print(Scanner[ip_add].all_protocols())
+    print("Open Ports: ", Scanner[ip_add]['tcp'].keys())
+    
+    
